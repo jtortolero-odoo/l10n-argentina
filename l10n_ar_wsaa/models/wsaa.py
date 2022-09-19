5b0395deb7b9d495a5e767b3f43fef71166fbf0b
Jose Gabriel Tortolero <gabriel.tortolero@eynes.com.ar>
2022-09-19 16:38:13 -0400

    wsaa

--- l10n_ar_wsaa/models/wsaa.py
+++ l10n_ar_wsaa/models/wsaa.py
@@ -14,38 +14,45 @@ class wsaa_config(models.Model):
     _name = "wsaa.config"
     _description = "Configuration for WSAA"
 
-    name = fields.Char('Description', size=255)
-    certificate = fields.Text('Certificate', readonly=True)
-    key = fields.Text('Private Key', readonly=True)
-    url = fields.Char('URL for WSAA', size=60, required=True)
+    name = fields.Char("Description", size=255)
+    certificate = fields.Text("Certificate", readonly=True)
+    key = fields.Text("Private Key", readonly=True)
+    url = fields.Char("URL for WSAA", size=60, required=True)
     company_id = fields.Many2one(
-        'res.company', 'Company Name', required=True,
-        default=lambda self: self.env.user.company_id.id)
-    service_ids = fields.One2many('wsaa.ta', 'config_id',
-                                  string="Authorized Services")
+        "res.company",
+        "Company Name",
+        required=True,
+        default=lambda self: self.env.user.company_id.id,
+    )
+    service_ids = fields.One2many("wsaa.ta", "config_id", string="Authorized Services")
 
     _sql_constraints = [
-        ('company_uniq',
-         'unique (company_id)',
-         'The configuration must be unique per company !')
+        (
+            "company_uniq",
+            "unique (company_id)",
+            "The configuration must be unique per company !",
+        )
     ]
 
     @api.multi
     def download_file(self):
         self.ensure_one()
-        field = self.env.context.get('field')
-        fname = self.env.context.get('filename')
+        field = self.env.context.get("field")
+        fname = self.env.context.get("filename")
         vals = {
-            'model': self._name,
-            'field': field,
-            'id': self.id,
-            'fname': fname,
+            "model": self._name,
+            "field": field,
+            "id": self.id,
+            "fname": fname,
         }
         action = {
-            'type': 'ir.actions.act_url',
-            'url': ('/web/binary/download_document?model=%(model)s&' +
-                    'field=%(field)s&id=%(id)s&filename=%(fname)s') % vals,
-            'target': 'new',
+            "type": "ir.actions.act_url",
+            "url": (
+                "/web/binary/download_document?model=%(model)s&"
+                + "field=%(field)s&id=%(id)s&filename=%(fname)s"
+            )
+            % vals,
+            "target": "new",
         }
         return action
 
@@ -54,13 +61,11 @@ class afipws_service(models.Model):
     _name = "afipws.service"
     _description = "WS Services"
 
-    name = fields.Char('Service Name', size=16, required=True)
-    description = fields.Text('Description')
+    name = fields.Char("Service Name", size=16, required=True)
+    description = fields.Text("Description")
 
     _sql_constraints = [
-        ('name_uniq',
-         'unique (name)',
-         'The name of the service must be unique!')
+        ("name_uniq", "unique (name)", "The name of the service must be unique!")
     ]
 
 
@@ -68,17 +73,19 @@ class wsaa_ta(models.Model):
     _name = "wsaa.ta"
     _description = "Ticket Access for WSAA"
 
-    name = fields.Many2one('afipws.service', 'Service')
-    token = fields.Text('Token', readonly=True)
-    sign = fields.Text('Sign', readonly=True)
-    expiration_time = fields.Char('Expiration Time', size=256)
-    config_id = fields.Many2one('wsaa.config')
-    company_id = fields.Many2one('res.company', 'Company Name')
+    name = fields.Many2one("afipws.service", "Service")
+    token = fields.Text("Token", readonly=True)
+    sign = fields.Text("Sign", readonly=True)
+    expiration_time = fields.Char("Expiration Time", size=256)
+    config_id = fields.Many2one("wsaa.config")
+    company_id = fields.Many2one("res.company", "Company Name")
 
     _sql_constraints = [
-        ('company_name_uniq',
-         'unique (name, company_id)',
-         'The service must be unique per company!')
+        (
+            "company_name_uniq",
+            "unique (name, company_id)",
+            "The service must be unique per company!",
+        )
     ]
 
     @api.model
@@ -86,36 +93,33 @@ class wsaa_ta(models.Model):
 
         wsaa_config = self.config_id
         service = self.name.name
-<<<<<<< HEAD
-        user = self.env['res.users'].browse(SUPERUSER_ID)
-=======
-        user = self.env['res.users'].sudo().browse(SUPERUSER_ID)
->>>>>>> 0a3efb23238b987f350a02bf4cba405f47bc23f4
+        user = self.env["res.users"].browse(SUPERUSER_ID)
         if user.partner_id.tz:
             tz = pytz.timezone(user.partner_id.tz)
         else:
             tz = pytz.utc
-        _wsaa = wsaa(wsaa_config.certificate, wsaa_config.key,
-                     wsaa_config.url, service, tz)
+        _wsaa = wsaa(
+            wsaa_config.certificate, wsaa_config.key, wsaa_config.url, service, tz
+        )
         _wsaa.get_token_and_sign(wsaa_config.certificate, wsaa_config.key)
 
         vals = {
-            'token': _wsaa.token,
-            'sign': _wsaa.sign,
-            'expiration_time': _wsaa.expiration_time.strftime(
-                '%Y-%m-%d %H:%M:%S'),
+            "token": _wsaa.token,
+            "sign": _wsaa.sign,
+            "expiration_time": _wsaa.expiration_time.strftime("%Y-%m-%d %H:%M:%S"),
         }
         return vals
 
     @api.multi
     def get_token_sign(self):
         ticket = self
-        force = self._context.get('force_renew', False)
+        force = self._context.get("force_renew", False)
 
         if not force:
             if ticket.expiration_time:
-                expiration_time = datetime.strptime(ticket.expiration_time,
-                                                    '%Y-%m-%d %H:%M:%S')
+                expiration_time = datetime.strptime(
+                    ticket.expiration_time, "%Y-%m-%d %H:%M:%S"
+                )
                 # Primero chequemos si tenemos ya un ticket expirado
                 # Si ahora+10 minutos es mayor al momento de expiracion
                 # debemos renovar el ticket.
@@ -129,12 +133,11 @@ class wsaa_ta(models.Model):
         vals = self._renew_ticket()
         with api.Environment.manage():
             new_cr = self.pool.cursor()
-            new_env = api.Environment(new_cr, self.env.user.id,
-                                      self.env.context)
-            new_env['wsaa.ta'].browse(self.id).write(vals)
+            new_env = api.Environment(new_cr, self.env.user.id, self.env.context)
+            new_env["wsaa.ta"].browse(self.id).write(vals)
             new_cr.commit()
             new_cr.close()
-        return vals['token'], vals['sign']
+        return vals["token"], vals["sign"]
 
     @api.multi
     def action_renew(self):
