82ae6164f22e6696e7ba2ef7eec873a2ab119225
Jose Gabriel Tortolero <gabriel.tortolero@eynes.com.ar>
2022-09-19 16:23:55 -0400

    vat ar

--- base_vat_ar/models/partner.py
+++ base_vat_ar/models/partner.py
@@ -10,49 +10,43 @@ from odoo.exceptions import ValidationError
 class DstCuitCodes(models.Model):
     _name = "dst_cuit.codes"
     _description = "DST CUIT Codes"
-    _order = 'name'
+    _order = "name"
 
-    code = fields.Float('Code', digits=(12, 0), required=True)
-    name = fields.Char('Desc', required=True, size=64)
+    code = fields.Float("Code", digits=(12, 0), required=True)
+    name = fields.Char("Desc", required=True, size=64)
 
 
 class ResDocumentType(models.Model):
     _name = "res.document.type"
-    _description = 'Document type'
+    _description = "Document type"
 
-    name = fields.Char(string='Document type', size=40)
-    afip_code = fields.Char(string='Afip code', size=10)
-    verification_required = fields.Boolean(
-        string='Verification required')
+    name = fields.Char(string="Document type", size=40)
+    afip_code = fields.Char(string="Afip code", size=10)
+    verification_required = fields.Boolean(string="Verification required")
     check_duplicated = fields.Boolean(string="Check Duplicated")
     dst_cuit = fields.Boolean(string="Enable DST CUIT")
 
-    def get_website_sale_documents(self, mode='billing'):
-        cuit = self.sudo().env.ref(
-            'base_vat_ar.document_cuit').name
-        cuil = self.sudo().env.ref(
-            'base_vat_ar.document_cuil').name
-        dni = self.sudo().env.ref(
-            'base_vat_ar.document_dni').name
-        return self.sudo().search([(
-            'name', 'in', (cuit, cuil, dni))])
+    def get_website_sale_documents(self, mode="billing"):
+        cuit = self.sudo().env.ref("base_vat_ar.document_cuit").name
+        cuil = self.sudo().env.ref("base_vat_ar.document_cuil").name
+        dni = self.sudo().env.ref("base_vat_ar.document_dni").name
+        return self.sudo().search([("name", "in", (cuit, cuil, dni))])
 
 
 class ResPartner(models.Model):
     _name = "res.partner"
     _inherit = "res.partner"
 
-    document_type_id = fields.Many2one('res.document.type',
-                                       string='Document type')
-    dst_cuit_id = fields.Many2one('dst_cuit.codes',
-                                  string='Country CUIT')
-    dst_cuit = fields.Boolean(string="Enable DST CUIT",
-                              related='document_type_id.dst_cuit')
+    document_type_id = fields.Many2one("res.document.type", string="Document type")
+    dst_cuit_id = fields.Many2one("dst_cuit.codes", string="Country CUIT")
+    dst_cuit = fields.Boolean(
+        string="Enable DST CUIT", related="document_type_id.dst_cuit"
+    )
 
-    @api.constrains('vat', 'document_type_id')
+    @api.constrains("vat", "document_type_id")
     def check_vat_duplicated(self):
         for partner in self:
-            if partner.env.context.get('from_website', False):
+            if partner.env.context.get("from_website", False):
                 continue
             if not partner.vat:
                 continue
@@ -60,17 +54,17 @@ class ResPartner(models.Model):
             to_check = document_type.check_duplicated
             if to_check:
                 search_param = [
-                    ('vat', '=', partner.vat),
-                    ('document_type_id', '=', document_type.id),
-                    ('parent_id', '=', False)
+                    ("vat", "=", partner.vat),
+                    ("document_type_id", "=", document_type.id),
+                    ("parent_id", "=", False),
                 ]
                 res = self.search_count(search_param)
                 if res > 1:
                     raise ValidationError(
-                        _('There is another partner with same ' +
-                          'VAT Information'))
+                        _("There is another partner with same " + "VAT Information")
+                    )
 
-    @api.onchange('document_type_id')
+    @api.onchange("document_type_id")
     def onchange_document_type(self):
         is_dst_cuit = self.document_type_id.dst_cuit
         if not is_dst_cuit:
@@ -87,20 +81,16 @@ class ResPartner(models.Model):
         vat = vals.get("vat")
         if not vat:
             return False
-        ci_extranjera = self.env.ref(
-            'base_vat_ar.document_ci_extranjera')
-        pasaporte = self.env.ref(
-            'base_vat_ar.document_pasaporte')
-        if [x for x in self if x.document_type_id not in [
-                ci_extranjera, pasaporte]]:
-            vat = vat.replace('.', '').replace('-', '')
+        ci_extranjera = self.env.ref("base_vat_ar.document_ci_extranjera")
+        pasaporte = self.env.ref("base_vat_ar.document_pasaporte")
+        if [x for x in self if x.document_type_id not in [ci_extranjera, pasaporte]]:
+            vat = vat.replace(".", "").replace("-", "")
             if not vat.isdigit():
-                raise ValidationError(
-                    _('The Vat only supports numbers.'))
-            vals['vat'] = vat
+                raise ValidationError(_("The Vat only supports numbers."))
+            vals["vat"] = vat
         return False
 
-    @api.constrains('vat', 'country_id', 'document_type_id')
+    @api.constrains("vat", "country_id", "document_type_id")
     def check_vat(self):
         """
         Check the VAT number depending of the country.
@@ -114,46 +104,37 @@ class ResPartner(models.Model):
             else:
                 try:
                     int(partner.vat[:2].lower())
-                    vat_country = 'ar'
+                    vat_country = "ar"
                     vat_number = partner.vat
                 except ValueError:
-                    vat_country, vat_number = partner.vat[:2].lower(), \
-                        partner.vat[2:].replace(' ', '')
-<<<<<<< HEAD
+                    vat_country, vat_number = partner.vat[:2].lower(), partner.vat[
+                        2:
+                    ].replace(" ", "")
             if not self.document_type_id:
-=======
-            if not partner.document_type_id:
->>>>>>> 0a3efb23238b987f350a02bf4cba405f47bc23f4
                 return True
-            if partner.document_type_id and \
-                    not partner.document_type_id.sudo().verification_required:
+            if (
+                partner.document_type_id
+                and not partner.document_type_id.sudo().verification_required
+            ):
                 return True
-<<<<<<< HEAD
-            if not hasattr(self, 'check_vat_' + vat_country):
+            if not hasattr(self, "check_vat_" + vat_country):
                 return True
-            check = getattr(self, 'check_vat_' + vat_country)
-=======
-            if not hasattr(partner, 'check_vat_' + vat_country):
-                return True
-            check = getattr(partner, 'check_vat_' + vat_country)
->>>>>>> 0a3efb23238b987f350a02bf4cba405f47bc23f4
+            check = getattr(self, "check_vat_" + vat_country)
             if not check(vat_number):
-                raise ValidationError(
-                    _('The Vat does not seems to be correct.'))
+                raise ValidationError(_("The Vat does not seems to be correct."))
         return True
 
     @api.model
     def _commercial_fields(self):
-        return super(ResPartner, self)._commercial_fields() + \
-            ['document_type_id']
+        return super(ResPartner, self)._commercial_fields() + ["document_type_id"]
 
     def check_vat_ar(self, vat):
         """
         Check VAT Routine for Argentina.
         """
-        cuit = self.sudo().env.ref('base_vat_ar.document_cuit')
-        cuil = self.sudo().env.ref('base_vat_ar.document_cuil')
-        dni = self.sudo().env.ref('base_vat_ar.document_dni')
+        cuit = self.sudo().env.ref("base_vat_ar.document_cuit")
+        cuil = self.sudo().env.ref("base_vat_ar.document_cuil")
+        dni = self.sudo().env.ref("base_vat_ar.document_dni")
         if self.document_type_id == cuit:
             if len(vat) != 11:
                 return False
