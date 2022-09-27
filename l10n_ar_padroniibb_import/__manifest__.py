95463476600f29eae55911140122de47cc9f8e5c
Jose Gabriel Tortolero <gabriel.tortolero@eynes.com.ar>
2022-09-27 17:12:27 -0400

    padron import py and view

--- l10n_ar_padroniibb_import/__manifest__.py
+++ l10n_ar_padroniibb_import/__manifest__.py
@@ -31,9 +31,9 @@
     ],
     "data": [
         "security/ir.model.access.csv",
-        #   "wizard/padron_import_view.xml",
+        "wizard/padron_import_view.xml",
         "wizard/padron_mass_update_view.xml",
-        #   "views/padron_view.xml",
+        "views/padron_view.xml",
         "views/perception_view.xml",
         "views/retention_view.xml",
     ],
--- l10n_ar_padroniibb_import/wizard/__init__.py
+++ l10n_ar_padroniibb_import/wizard/__init__.py
@@ -3,5 +3,5 @@
 #   License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 ##############################################################################
 
-# from . import padron_import  # noqa
+from . import padron_import  # noqa
 from . import padron_mass_update  # noqa
